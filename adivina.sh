b48b9cafa506d502ec8e2006a9b2ed3613421ce5
enti <enti@enti.enti>
2022-10-26 10:30:44 +0200

    Adivina bien guapo

--- adivina.sh
+++ adivina.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Adivina el numero del 1 al 100"
-
-read NUM
-
-if [ "$NUM" -eq "3" ]
-then
-	echo "Son iguales"
-fi
+NUMERO_RANDOM=$(( $RANDOM % 100 + 1))
+NUM=0
+while [ $NUM -ne $NUMERO_RANDOM ]
+do
+	echo "Adivina el numero del 1 al 100"
+	read NUM
+	if [ "$NUM" -gt $NUMERO_RANDOM ]
+	then
+		echo "Es menor"
+	elif [ $NUM -lt $NUMERO_RANDOM ]
+	then
+		echo "Es mayor"
+	else
+		echo "Correcto" 
+	fi
+done
