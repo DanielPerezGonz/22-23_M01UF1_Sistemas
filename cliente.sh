87ba69b1d9ad1ce5d004feb13413ea9e12d226b6
Dani Perez <daniel.perez@enti.cat>
2022-11-09 10:45:30 +0100

    Actualizacion de cliente y servidor

--- cliente.sh
+++ cliente.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
+SERVER_AD="localhost"
+
+PORT="2223"
+
 echo "Cliente TURIP"
 
 echo "(1) SEND MSG: HOLI_TURIP"
-echo "HOLI_TURIP 127.0.0.1" | nc localhost 2223
+
+echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_AD $PORT
+
+echo "(2) LISTEN: Comprobacion Handshake"
+
+MSG=`nc -l $PORT`
+
+echo $MSG
+
+if [ "$MSG" != "OK_TURIP" ]
+then
+	echo "ERROR 1: Handshake incorrecto"
+	exit 1
+fi
+
+echo "(5) SEND MSG"
+
+echo "FILE_NAME IP.IP" | nc $SERVER_AD $PORT
+
+echo "(6) LISTEN"
+
+MSG=`nc -l $PORT`
+
+echo $MSG
+
+if [ "$MSG" != "OK_TURIP" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	exit 2
+fi
+
+
+
+exit 0
--- servidor.sh
+++ servidor.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
+PORT="2223"
+
 echo "Servidor Transfer Unite Recursive Internationel Protocol: TURIP"
 
 echo "(0) LISTEN"
-HS=(`nc -l 2223`) | (cut -d " " -f 1)
-IP=(`nc -l 2223`) | (cut -d " " -f 2)
+MSG=`nc -l $PORT`
+HS=`echo $MSG | cut -d " " -f 1`
+IP=`echo $MSG | cut -d " " -f 2`
 
+echo "(3) SEND: Comprobacion"
 if [ "$HS" != "HOLI_TURIP" ]
 then 
 	echo "ERROR 1: Handshake incorrecto"
+	echo "KO_TURIP" | nc $IP $PORT
 	exit 1
 fi
+
+echo "OK_TURIP" | nc $IP $PORT
+
+echo "(4) LISTEN"
+
+MSG=`nc -l $PORT`
+PRF=`echo $MSG | cut -d " " -f 1`
+FN=`echo $MSG | cut -d " " -f 2`
+
+echo "(7) SEND: Comprobacion"
+if [ "$PRF" != "FILE_NAME" ]
+then
+	echo "ERROR 2: Prefijo incorrecto"
+	echo "KO_TURIP" | nc $IP $PORT
+	exit 2
+fi
+
+echo "OK_TURIP" | nc $IP $PORT
+
+
+
+
+
+
+
+
+
+exit 0
